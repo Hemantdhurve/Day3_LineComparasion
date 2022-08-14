fb0c9789ac1f1a7e64e7748fa2b26eab84aad039
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-14 19:29:46 +0530

    Added UC3 to check two line greater or less than another line

---
+++ LineComparisionProb/LineComparisionProb/LineComparasion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProb
+{
+    public class LineComparasion
+    {
+        public static void CalculateLength()
+        {
+            //comparing two lines L1 and L2
+
+            //1st part line no 1 : L1
+
+            Console.WriteLine("\t\tEnter values for Line No 1: ");
+
+            Console.WriteLine("Enter Value of : X1");
+            //string value converted to integer
+            int X1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : X2");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y1");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y2");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            //2st part line no 2 : L2
+
+            Console.WriteLine("\t\tEnter values for Line No 2: ");
+
+
+            Console.WriteLine("Enter Value of : X3");
+            int X3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : X4");
+            int X4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y3");
+            int Y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y4");
+            int Y4 = Convert.ToInt32(Console.ReadLine());
+
+            //using double to get square root value in double decimal points
+
+            //to compare two lines we need 2 lengths as L1 and L2
+            double Length1 = Math.Sqrt((Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)));
+            Console.WriteLine("Length of the line is :" + Length1);
+
+            double Length2 = Math.Sqrt((Math.Pow((X4 - X3), 2) + Math.Pow((Y4 - Y3), 2)));
+            Console.WriteLine("Length of the line is :" + Length2);
+            Console.ReadLine();
+
+            //using sequence selection to check lines are equal or not 
+            if (Length1 == Length2)
+            {
+                Console.WriteLine("BOTH LINES ARE EQUAL");
+            }
+
+            else if (Length1 > Length2)
+            {
+                Console.WriteLine("L1 IS GREATER THAN L2");
+            }
+            else
+            {
+                Console.WriteLine("L1 IS LESS THAN L2");
+            }
+        }
+    }
+}
--- LineComparisionProb/LineComparisionProb/Program.cs
+++ LineComparisionProb/LineComparisionProb/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using LineComparisionProb;
+using System;
 
-namespace LineComparisonProb
+namespace LineComparasionProb
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Problem");
+            Console.WriteLine("\t\tWelcome to Line Comparision Problem");
+
+            //using switch case to 
+            Console.WriteLine("\t\tOutput as to Check Equality of Two Lines which is greater or less  :");
+
+            //method is static so method can be called using className 
+
+            LineComparasion.CalculateLength();
+
         }
     }
 }
